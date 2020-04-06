92d46ec06a33db0f1472eb19c738ad0bdeace00b
Saif Hussain <sh795@kent.ac.uk>
2020-04-06 19:05:44 +0100

    Iterate over images

--- ImageAI/FirstPrediction.py
+++ ImageAI/FirstPrediction.py
@@ -1,50 +1,81 @@
-# Importing classing
-from imageai.Prediction import ImagePrediction
-import os
+# # Importing classing
+# from imageai.Prediction import ImagePrediction
+# import os
+#
+# # INFO, WARNING, and ERROR messages are not printed
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+#
+# # Set execution path as variable so we can access algorithms and images
+# execution_path = os.getcwd()
+#
+# def debugFile():
+#     # path = "/Users/Saif/Desktop/ImageAI/"
+#     # filepath = execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5"
+#     # print(execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5")
+#     filepath = execution_path
+#     # path = os.path.join(filepath).replace(" ", "\\ ")
+#     files = os.listdir(filepath)
+#     for name in files:
+#         print(name)
+#
+# filepath = execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5"
+# # print(execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5")
+#
+# # path = os.path.join(filepath).replace(" ", "\\ ")
+# # print(path)
+#
+#
+# def prediction():
+#     # Create instance of image prediction class
+#     prediction = ImagePrediction()
+#
+#     # Set the model type of the prediction object and path - basically determines which algorithm we are using
+#     # prediction.setModelTypeAsResNet()
+#     prediction.setModelTypeAsInceptionV3()
+#     # prediction.setModelPath(execution_path + "/" + "resnet50_weights_tf_dim_ordering_tf_kernels.h5")
+#     # print(execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5")
+#     # prediction.setModelPath(path)
+#     prediction.setModelPath(filepath)
+#     prediction.loadModel()
+#
+#     # Return 2 array objects with the predictions (result_count) and the percentage probabilities
+#     global predictions
+#     predictions, percentage_probabilities = prediction.predictImage(execution_path + "/" + "pizza.jpg", result_count=5)
+#     # Iterate through objects and prints percentages per prediction
+#     for index in range(len(predictions)):
+#         print(predictions[index], " : ", percentage_probabilities[index])
+#
+#
+# debugFile()
+# prediction()
 
-# INFO, WARNING, and ERROR messages are not printed
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from PIL import Image
 
-# Set execution path as variable so we can access algorithms and images
-execution_path = os.getcwd()
+import cloudsight
 
-def debugFile():
-    # path = "/Users/Saif/Desktop/ImageAI/"
-    # filepath = execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5"
-    # print(execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5")
-    filepath = execution_path
-    # path = os.path.join(filepath).replace(" ", "\\ ")
-    files = os.listdir(filepath)
-    for name in files:
-        print(name)
+# im = Image.open('sample.jpg')
+# im.thumbnail((600,600))
+# im.save('cloudsight.jpg')
 
-filepath = execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5"
-# print(execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5")
+auth = cloudsight.SimpleAuth('gqPvkvZfv_mAwMO3Kaa9Zw')
 
-# path = os.path.join(filepath).replace(" ", "\\ ")
-# print(path)
+api = cloudsight.API(auth)
 
+for x in range(5):
+    print(x)
+    string = str(x)
+    im = Image.open(string + '.jpg')
+    im.thumbnail((600, 600))
+    im.save(string + 'cloudsight.jpg')
+    with open(string + '.jpg', 'rb') as f:
+        response = api.image_request(f, string + 'cloudsight.jpg', {'image_request[locale]': 'en-US',})
+        status = api.wait(response['token'], timeout=30)
 
-def prediction():
-    # Create instance of image prediction class
-    prediction = ImagePrediction()
+    print(status['name'])
 
-    # Set the model type of the prediction object and path - basically determines which algorithm we are using
-    # prediction.setModelTypeAsResNet()
-    prediction.setModelTypeAsInceptionV3()
-    # prediction.setModelPath(execution_path + "/" + "resnet50_weights_tf_dim_ordering_tf_kernels.h5")
-    # print(execution_path + "/" + "inception_v3_weights_tf_dim_ordering_tf_kernels.h5")
-    # prediction.setModelPath(path)
-    prediction.setModelPath(filepath)
-    prediction.loadModel()
-
-    # Return 2 array objects with the predictions (result_count) and the percentage probabilities
-    global predictions
-    predictions, percentage_probabilities = prediction.predictImage(execution_path + "/" + "pizza.jpg", result_count=5)
-    # Iterate through objects and prints percentages per prediction
-    for index in range(len(predictions)):
-        print(predictions[index], " : ", percentage_probabilities[index])
-
-
-debugFile()
-prediction()
+# with open('cloudsight.jpg', 'rb') as f:
+#     response = api.image_request(f, 'cloudsight.jpg', {'image_request[locale]': 'en-US',})
+#
+# status = api.wait(response['token'], timeout=30)
+#
+# print(status['name'])
